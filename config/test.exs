efc8fe22b85deae279eb174a62807c3872500017
trepkosh <mrlewap@gmail.comm>
2020-04-02 16:24:32 +0600

    dockerize dev environment

--- config/test.exs
+++ config/test.exs
@@ -3,9 +3,9 @@ use Mix.Config
 # Configure your database
 config :cookpad, Cookpad.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "",
   database: "cookpad_test",
-  hostname: "localhost",
+  hostname: "db",
   pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
