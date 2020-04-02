48745084d8f3fd373f64bc1a4e09bc992811bcdd
trepkosh <mrlewap@gmail.comm>
2020-04-02 17:26:01 +0600

    fix test postgres hostname for github

--- config/test.exs
+++ config/test.exs
@@ -10,7 +10,12 @@ config :cookpad, Cookpad.Repo,
 
 # Configure the database for GitHub Actions
 if System.get_env("GITHUB_ACTIONS") do
-  config :app, Cookpad.Repo, hostname: "localhost"
+  config :cookpad, Cookpad.Repo,
+    username: "postgres",
+    password: "postgres",
+    database: "cookpad_test",
+    hostname: "localhost",
+    pool: Ecto.Adapters.SQL.Sandbox
 end
 
 # We don't run a server during test. If one is required,
