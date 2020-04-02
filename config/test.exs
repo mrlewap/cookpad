4023f3676e50f3e8a4843c593ee66d68c17fc22c
trepkosh <mrlewap@gmail.comm>
2020-04-02 16:40:20 +0600

    add test db for github actions

--- config/test.exs
+++ config/test.exs
@@ -8,6 +8,13 @@ config :cookpad, Cookpad.Repo,
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
+# Configure the database for GitHub Actions
+if System.get_env("GITHUB_ACTIONS") do
+  config :app, Cookpad.Repo,
+         username: "postgres",
+         password: "postgres"
+end
+
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :cookpad, CookpadWeb.Endpoint,
