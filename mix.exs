d439e7ccadf06f94b99d34e0ddf2f8c2f22689ef
trepkosh <mrlewap@gmail.comm>
2020-04-02 17:43:16 +0600

    add dialyzer

--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule Cookpad.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:credo, "~> 1.2", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.2", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
     ]
   end
 
